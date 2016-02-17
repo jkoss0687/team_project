fb6fdf46b64ea7bf061954d1479daa8ecd4ba38b
Patrick <topolski.patrick@gmail.com>
2016-02-17 16:14:20 -0500

    add name to on click seat

--- seat-res-starter/app.js
+++ seat-res-starter/app.js
@@ -1,16 +1,27 @@
  $(document).ready(function () {
+      var currentSeat;
       var bHeight = $("body").height();
       var vpHeight = $(window).height();
       if (vpHeight > bHeight) {
         $("footer#sticky").css("position","absolute").css("bottom",0);
       };
- });
 
- $(document).ready(function () {
  $(".available").on('click', function(){
     $('.form-Toggle').slideToggle('slow')
-    $(this).toggleClass('reserved');
-  });   
+    $(this).toggleClass('reserved')
+      currentSeat = $(this);
+   });   
+ $('button').on('click', function(e){
+    e.preventDefault();
+      var inputs = $('form').serializeArray();
+      var formObj = {};
+      $.each(inputs, function (i, input) {
+        formObj[input.name] = input.value;
+      });
+      currentSeat.prepend(formObj.firstname, '<br>', formObj.lastname);
+      
  });
+ });
+
 
 
